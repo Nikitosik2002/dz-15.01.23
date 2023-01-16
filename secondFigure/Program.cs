28b27bbfcef226faf7b473519c5ee57dc6de0e00
Nikitosik <nikita.kopeickin@yandex.ru>
2023-01-16 10:26:08 +0300

    second figure of number

---
+++ secondFigure/Program.cs
@@ -0,0 +1,6 @@
+﻿System.Console.WriteLine("Введите трехзначное число, чтобы найти его вторую цифру");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int figure = number % 100;
+figure = figure / 10;
+System.Console.WriteLine(figure);
