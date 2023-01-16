afacf5bde838eed76e546783bc286580847a7aa9
Nikitosik <nikita.kopeickin@yandex.ru>
2023-01-16 10:42:23 +0300

    third figure

---
+++ thirdFigure/Program.cs
@@ -0,0 +1,16 @@
+﻿System.Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+string third = number.ToString();
+
+if (number > 99)
+{
+    System.Console.WriteLine("Третья цифра вашего числа равна " + third[2]);
+}
+else
+{
+    System.Console.WriteLine("Число не имеет третьего разряда");
+}
+
+
+
