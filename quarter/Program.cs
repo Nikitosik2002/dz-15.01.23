56e10f584a1f37a894b173c06151c39b78c71cb2
Nikitosik <nikita.kopeickin@yandex.ru>
2023-01-16 11:12:43 +0300

    inf about quarter

---
+++ quarter/Program.cs
@@ -0,0 +1,27 @@
+﻿System.Console.WriteLine("Задайте номер четверти от 1 до 4");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number == 1)
+{
+    System.Console.WriteLine("Четверть номер 1, в ней X > 0 и Y > 0");
+}
+
+else if (number == 2)
+{
+    System.Console.WriteLine("Четверть номер 2, в ней X < 0 а Y > 0");
+}
+
+else if (number == 3)
+{
+    System.Console.WriteLine("Четверть номер 3, в ней X < 0 и Y < 0");
+}
+
+else if (number == 4)
+{
+    System.Console.WriteLine("Четверть номер 4, в ней X > 0 а Y < 0");
+}
+
+else
+{
+    System.Console.WriteLine("Вы ввели несуществующий номер четверти попробуйте снова");
+}
