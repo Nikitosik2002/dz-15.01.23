6e27544f87e43912db6e6b794da2612854a5be36
Nikitosik <nikita.kopeickin@yandex.ru>
2023-01-16 10:20:52 +0300

    palindrom lesson

---
+++ polindrom/Program.cs
@@ -0,0 +1,23 @@
+﻿System.Console.WriteLine("Введите пятизначное число, для проверки на палиндром");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int first = number / 10000;
+
+int last = number % 10;
+
+int second = number / 1000;
+second = second % 10;
+
+int per_last = number % 100;
+per_last = per_last / 10;
+
+
+if (first == last & second == per_last)
+{
+    System.Console.WriteLine("Число является палиндромом");
+}
+else
+{
+    System.Console.WriteLine("Число не является палиндромом");
+}
+
