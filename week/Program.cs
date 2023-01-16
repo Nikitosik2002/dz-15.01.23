e7423417af6326d55f274cf6d18e1f2c562c4e90
Nikitosik <nikita.kopeickin@yandex.ru>
2023-01-16 10:50:40 +0300

    number of week

---
+++ week/Program.cs
@@ -0,0 +1,15 @@
+﻿System.Console.WriteLine("Введите цифру обозначающую день недели");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number >= 1 & number < 6)
+{
+    System.Console.WriteLine("Сегодня рабочий день");
+}
+else if (number > 5 & number < 8)
+{
+    System.Console.WriteLine("Сегодня выходной");
+}
+else
+{
+    System.Console.WriteLine("Вы ввели неправильную цифру дня, попробуйте снова");
+}
